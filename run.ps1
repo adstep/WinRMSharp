78298d89d884a4d67800e3947f66be9567ac57e3
adstep <stephensonadamj@gmail.com>
2022-12-12 21:02:31 -0800

    remove run script from root

--- run.ps1
+++
@@ -1,8 +0,0 @@
-dotnet test .\src\WinRMSharp.Tests\ --framework net7.0 --collect:"XPlat Code Coverage"
-
-$coverageFiles = Get-ChildItem -Path "$PSScriptRoot/**/coverage.cobertura.xml" -Recurse
-$coverageFiles = $coverageFiles | Sort-Object -Property LastWriteTime -Descending
-
-$latestCoverageFile = $coverageFiles[0]
-
-reportgenerator.exe -reports:$latestCoverageFile -targetdir:"coveragereport" -reporttypes:Html
